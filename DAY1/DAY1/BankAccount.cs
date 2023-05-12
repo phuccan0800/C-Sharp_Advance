0ef1567e1bcfcb310a568687fc1149212185b603
Truong Tan Phuc <phuccan0800@gmail.com>
2023-05-12 13:12:26 +0700

    Add project files.

---
+++ DAY1/DAY1/BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DAY1
+{
+    internal class BankAccount
+    {
+        private int id;
+        private decimal balance;
+        public int Id { get { return id; } set { id = Id; } }
+        public decimal Balance { get { return balance; } set { balance = value; } }
+        public void Deposit(decimal amount)
+        {
+            this.Balance += amount;
+        }
+        public void Withdraw(decimal amount)
+        {
+            this.Balance -= amount;
+        }
+
+        public override string ToString()
+        {
+            return $"Problem2: Account {this.Id}, balance {this.Balance}";
+        }
+    }
+}
---
+++ DAY1/DAY1/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DAY1
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount();
+            account.Id = 1;
+            account.Balance = 15;
+            Console.WriteLine($"Problem1: Account {account.Id}, balance {account.Balance} ");
+            account.Id = 1;
+            account.Deposit(15);
+            account.Withdraw(10);
+            account.ToString();
+        }
+    }
+}
